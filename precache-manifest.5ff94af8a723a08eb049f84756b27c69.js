0296acda50fb9347f16bea4603c929b0c24218ea
David N <bigair_1080@hotmail.com>
2019-09-23 00:18:11 -0300

    Updates

---
+++ precache-manifest.5ff94af8a723a08eb049f84756b27c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c055b2a7705998b8fd85b7e25a8855d",
+    "url": "/plot-generator/index.html"
+  },
+  {
+    "revision": "ee92e1ef65df6eeffcde",
+    "url": "/plot-generator/static/js/2.b0cf6542.chunk.js"
+  },
+  {
+    "revision": "fda05b9414e75563ec04",
+    "url": "/plot-generator/static/js/main.6adda50c.chunk.js"
+  },
+  {
+    "revision": "5237578be9d8bcb21621",
+    "url": "/plot-generator/static/js/runtime~main.6711312d.js"
+  },
+  {
+    "revision": "71793c6284fc455bc619bcdd495a9f7e",
+    "url": "/plot-generator/static/media/close.71793c62.svg"
+  },
+  {
+    "revision": "2fcb3dc23c4fda23a0d2a0f1eb304802",
+    "url": "/plot-generator/static/media/refresh.2fcb3dc2.svg"
+  },
+  {
+    "revision": "0213d47b19605665668020894cda8891",
+    "url": "/plot-generator/static/media/trash.0213d47b.svg"
+  }
+]);
