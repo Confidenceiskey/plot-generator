20a8d0b9acb916c97d5e05f1b39e6f6ef5fc579f
David N <bigair_1080@hotmail.com>
2021-01-05 20:00:24 +0100

    Updates

---
+++ precache-manifest.93d958e3038b650a116f91eb2660e94b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d2a0eadb2548410e6d73ea282bfcd4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3e87ca720add1997642",
+    "url": "./static/js/2.e16f0492.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.e16f0492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0a47648b98917d273c",
+    "url": "./static/js/main.8aa1ac94.chunk.js"
+  },
+  {
+    "revision": "3d1ccd98b04e93f85fb7",
+    "url": "./static/js/runtime-main.93e18156.js"
+  },
+  {
+    "revision": "71793c6284fc455bc619bcdd495a9f7e",
+    "url": "./static/media/close.71793c62.svg"
+  },
+  {
+    "revision": "2fcb3dc23c4fda23a0d2a0f1eb304802",
+    "url": "./static/media/refresh.2fcb3dc2.svg"
+  },
+  {
+    "revision": "0213d47b19605665668020894cda8891",
+    "url": "./static/media/trash.0213d47b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/plot-generator/precache-manifest.b7c5592adc75ecb1d6033c8bd2945dfa.js"
+  "./precache-manifest.93d958e3038b650a116f91eb2660e94b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plot-generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
